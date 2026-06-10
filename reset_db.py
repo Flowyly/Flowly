bfd54a0a425b3ca350555f53c35c7bc45796c9da
KHALED <kaldinkhaled@gmail.com>
2026-06-10 22:03:15 +0800

    correction

--- reset_db.py
+++
@@ -1,7 +0,0 @@
-from backend import db, app
-
-with app.app_context():
-    db.drop_all()   # deletes old tables
-    db.create_all() # creates fresh correct tables
-
-print("Database reset complete 🚀")
